ceddf125ac4f7334202cd2307a936a1eb3066a56
mrjordanov <marin.jordanov@gmail.com>
2022-05-04 16:51:37 +0300

    Intro to Hibernate

---
+++ 3.Hibernate Intro/src/main/java/Main.java
@@ -0,0 +1,24 @@
+import entities.Student;
+import entities.Teacher;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("school");
+        EntityManager entityManager = emf.createEntityManager();
+        entityManager.getTransaction().begin();
+
+        Student student = new Student("emo");
+        Teacher teacher = new Teacher("petka", LocalDate.now());
+        entityManager.persist(student);
+        entityManager.persist(teacher);
+        teacher.setName("Gerg");
+        entityManager.persist(teacher);
+        entityManager.getTransaction().commit();
+        entityManager.close();
+    }
+}
